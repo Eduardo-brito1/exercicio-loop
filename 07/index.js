79a47699fb55d341be45572f70ec20739e635ee0
Eduardo-Brito1 <kabisashi2@gmail.com>
2024-02-01 18:28:27 -0300

    exercicio feito

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,12 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+
+
+const somenteComA = []
+
+for (let i = 0; i < nomes.length; ++i) {
+    if (nomes[i].indexOf('A', 0) === 0 || nomes[i].indexOf('a', 0) === 0) {
+        somenteComA.push(nomes[i])
+    }
+}
+
+console.log(somenteComA);
