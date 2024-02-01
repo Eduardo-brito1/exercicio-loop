56d0665d0a448fafc9dc3a824a3af7b0d90601fc
Eduardo-Brito1 <kabisashi2@gmail.com>
2024-02-01 19:00:06 -0300

    exercicio feito

--- 11/index.js
+++ 11/index.js
@@ -1,2 +1,11 @@
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
+const newArray = []
 
+for (let i = 0; i < original.length; i++) {
+
+    if (original[i] > 10 && original[i] <= 20 || original[i] > 100) {
+        newArray.push(original[i])
+    }
+}
+
+console.log(newArray)
