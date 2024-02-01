8a84b3f3472a85425a743d6c5f861111b3ad23b6
Eduardo-Brito1 <kabisashi2@gmail.com>
2024-02-01 19:05:48 -0300

    exercicio feito

--- 12/index.js
+++ 12/index.js
@@ -1,3 +1,13 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+let indice = 0
+
+while (filaDeFora.length >= filaDeDentro.length) {
+    filaDeDentro.push(filaDeFora[indice])
+
+    indice++;
+}
+
+
+console.log(filaDeDentro)
