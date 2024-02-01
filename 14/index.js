46b2a751327388249f0b45bf0c12f10bc4be764e
Eduardo-Brito1 <kabisashi2@gmail.com>
2024-02-01 20:04:37 -0300

    exercicio feito

--- 14/index.js
+++ 14/index.js
@@ -1,3 +1,19 @@
 const arrayA = [5, 32, 3, 44, 1];
 const arrayB = [57, 4, 21, 2, 13];
 
+let menor = 0
+
+for (let i = 0; i < arrayA.length; i++) {
+
+    if (arrayA[i] < arrayB[i]) {
+        menor = arrayA[i]
+        console.log(menor)
+    }
+
+    if (arrayB[i] < arrayA[i]) {
+        menor = arrayB[i]
+        console.log(menor)
+    }
+
+}
+
